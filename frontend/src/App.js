f3367de8f76a49ff21cc698a80af660d7daa1f9a
huy2122k <wwwhuy212@gmail.com>
2023-03-16 23:32:30 +0700

    images

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -324,7 +324,7 @@ function App() {
                           
                           <BotResponse response={chat.botMessage} />
 
-                        {/* <GenerateImage/> */}
+                        <GenerateImage/>
                           <Speak
                             text={chat.botMessage}
                             startBtn={
